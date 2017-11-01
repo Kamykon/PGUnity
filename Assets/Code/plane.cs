93b9a0c6bb44d030b74b26074ab5a234176cd538
Michal Kaminski <michalkam12@gmail.com>
2017-11-01 20:56:51 +0100

    more flight time using slerp

--- Assets/Code/plane.cs
+++ Assets/Code/plane.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class plane : MonoBehaviour {
 
     private float startTime;
+    int flightOrder = 1;
 
     // Use this for initialization
     void Start () {
@@ -13,15 +14,70 @@ public class plane : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if(flightOrder == 1)
+        {
+            oneToTwo();
+        }
+        if(flightOrder == 2)
+        {
+            twoToThree();
+        }
+        if (flightOrder == 3)
+        {
+            threeToFour();
+        }
+    }
+
+    void oneToTwo()
+    {
         var pos1 = GameObject.Find("lotnisko1").transform.position;
         var pos2 = GameObject.Find("lotnisko2").transform.position;
         Vector3 center = (pos1 + pos2);
         center += new Vector3(0, 8, 0);
         float fracComplete = (Time.time - startTime) / 2;
-        Vector3 riseRelCenter = pos1 - center;
-        Vector3 setRelCenter = pos2 - center;
+        Vector3 start = pos1 - center;
+        Vector3 finish = pos2 - center;
 
-        transform.position = Vector3.Slerp(riseRelCenter, setRelCenter, fracComplete);
+        transform.position = Vector3.Slerp(start, finish, fracComplete);
+        transform.position += center;
+        if(transform.position == pos2)
+        {
+            flightOrder = 2;
+            startTime = Time.time;
+        }
+    }
+
+    void twoToThree()
+    {
+        var pos1 = GameObject.Find("lotnisko2").transform.position;
+        var pos2 = GameObject.Find("lotnisko3").transform.position;
+        Vector3 center = (pos1 + pos2);
+        center += new Vector3(0, 12, 0);
+        float fracComplete = (Time.time - startTime) / 2;
+        Vector3 start = pos1 - center;
+        Vector3 finish = pos2 - center;
+
+        transform.position = Vector3.Slerp(start, finish, fracComplete);
+        transform.position += center;
+
+        if (transform.position == pos2)
+        {
+            flightOrder = 3;
+            startTime = Time.time;
+        }
+    }
+
+    void threeToFour()
+    {
+        var pos1 = GameObject.Find("lotnisko3").transform.position;
+        var pos2 = GameObject.Find("lotnisko4").transform.position;
+        Vector3 center = (pos1 + pos2);
+        center += new Vector3(12, 0, 0);
+        float fracComplete = (Time.time - startTime) / 2;
+        Vector3 start = pos1 - center;
+        Vector3 finish = pos2 - center;
+
+        transform.position = Vector3.Slerp(start, finish, fracComplete);
         transform.position += center;
     }
 }
