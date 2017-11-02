6cf97836dc5b07c0a6d60a9e41e05fc30670d087
Miłosz Szyrner <milosz.szyrner@gmail.com>
2017-11-02 12:17:06 +0100

    Bezier curve movement example

--- Assets/Code/plane.cs
+++ Assets/Code/plane.cs
@@ -1,46 +1,64 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class plane : MonoBehaviour {
 
     private float startTime;
-    int flightOrder = 1;
+    private int numPoints = 500;
+    int flightOrder = 2;
+    public float speed = 1f;
 
+    GameObject firstAirport;
+    GameObject secondAirport;
+    GameObject thirdAirport;
+    GameObject fourthAirport;
+    GameObject planeg;
+
+    Vector3 firstAirportPosition;
+    Vector3 secondAirportPosition;
+
+    Vector3 firstAirportBoxColliderPosition;
+    Vector3 secondAirportBoxColliderPosition;
+
+    Vector3 center;
+    Vector3 start;
+    Vector3 finish;
     // Use this for initialization
     void Start () {
         startTime = Time.time;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if(flightOrder == 1)
+        firstAirport = GameObject.Find("lotnisko1");
+        secondAirport = GameObject.Find("lotnisko2");
+        thirdAirport = GameObject.Find("lotnisko3");
+        fourthAirport = GameObject.Find("lotnisko4");
+        planeg = GameObject.Find("New Sprite");
+        if (flightOrder == 1)
         {
-            oneToTwo();
+           oneToTwo();
         }
-        if(flightOrder == 2)
+        if (flightOrder == 2)
         {
-            twoToThree();
+           twoToThree();
         }
         if (flightOrder == 3)
         {
             threeToFour();
         }
     }
+	
+	// Update is called once per frame
+	void Update () {
+      
+    }
 
     void oneToTwo()
     {
-        var pos1 = GameObject.Find("lotnisko1").transform.position;
-        var pos2 = GameObject.Find("lotnisko2").transform.position;
-        Vector3 center = (pos1 + pos2);
-        center += new Vector3(0, 8, 0);
-        float fracComplete = (Time.time - startTime) / 2;
-        Vector3 start = pos1 - center;
-        Vector3 finish = pos2 - center;
-
-        transform.position = Vector3.Slerp(start, finish, fracComplete);
-        transform.position += center;
-        if(transform.position == pos2)
+        firstAirportPosition = firstAirport.transform.position;
+        secondAirportPosition = secondAirport.transform.position;
+        Vector3[] positions = getBezier(firstAirportPosition, firstAirportPosition + new Vector3(3f, -11f, 0), secondAirportPosition - new Vector3(11f, 0f, 0f), secondAirportPosition);
+        StartCoroutine(movePlane(positions));
+        if (transform.position == secondAirportPosition)
         {
             flightOrder = 2;
             startTime = Time.time;
@@ -49,35 +67,61 @@ public class plane : MonoBehaviour {
 
     void twoToThree()
     {
-        var pos1 = GameObject.Find("lotnisko2").transform.position;
-        var pos2 = GameObject.Find("lotnisko3").transform.position;
-        Vector3 center = (pos1 + pos2);
-        center += new Vector3(0, 12, 0);
-        float fracComplete = (Time.time - startTime) / 2;
-        Vector3 start = pos1 - center;
-        Vector3 finish = pos2 - center;
+        firstAirportPosition = secondAirport.transform.position;
+        secondAirportPosition = fourthAirport.transform.position;
+        Vector3[] positions = getBezier(firstAirportPosition, firstAirportPosition - new Vector3(18f, 0, 0), secondAirportPosition - new Vector3(22f, 0f, 0f), secondAirportPosition);
+        StartCoroutine(movePlane(positions));
+    }
 
-        transform.position = Vector3.Slerp(start, finish, fracComplete);
-        transform.position += center;
-
-        if (transform.position == pos2)
+    private IEnumerator movePlane(Vector3[] positions)
+    {
+        for (int i = 0; i < numPoints; i++)
         {
-            flightOrder = 3;
-            startTime = Time.time;
+            float step = speed * Time.deltaTime;
+            transform.position = positions[i];
+            yield return new WaitForSeconds(step);
         }
     }
 
     void threeToFour()
     {
-        var pos1 = GameObject.Find("lotnisko3").transform.position;
-        var pos2 = GameObject.Find("lotnisko4").transform.position;
-        Vector3 center = (pos1 + pos2);
-        center += new Vector3(12, 0, 0);
+        firstAirportPosition = thirdAirport.transform.position;
+        secondAirportPosition = fourthAirport.transform.position;
+        firstAirportBoxColliderPosition = thirdAirport.GetComponent<BoxCollider2D>().bounds.max;
+        secondAirportBoxColliderPosition = fourthAirport.GetComponent<BoxCollider2D>().bounds.min;
+        secondAirportBoxColliderPosition += new Vector3(0, secondAirport.GetComponent<BoxCollider2D>().bounds.size.y / 2, 0);
+        Vector3 center = (firstAirportPosition + secondAirportPosition);
         float fracComplete = (Time.time - startTime) / 2;
-        Vector3 start = pos1 - center;
-        Vector3 finish = pos2 - center;
+        Vector3 start = firstAirportPosition - center;
+        Vector3 finish = secondAirportBoxColliderPosition - center;
 
         transform.position = Vector3.Slerp(start, finish, fracComplete);
         transform.position += center;
+        transform.position = Vector3.MoveTowards(transform.position, secondAirportPosition, fracComplete);
+    }
+
+    private Vector3[] getBezier(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+    {
+        Vector3[] positions = new Vector3[numPoints];
+        for(int i = 1; i < numPoints; i++)
+        {
+            float t = i / (float)numPoints;
+            positions[i] = calculateBubicBezierPoint(t, p0, p1, p2, p3);
+        }
+        return positions;
+    }
+
+    private Vector3 calculateBubicBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3 )
+    {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+        float uuu = uu * u;
+        float ttt = tt * t;
+        Vector3 p = uuu * p0;
+        p += 3 * uu * t * p1;
+        p += 3 * u * tt * p2;
+        p += ttt * p3;
+        return p;
     }
 }
