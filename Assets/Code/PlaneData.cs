323a85dd6de1886179be1e0788ef95dffd766a20
Michal Kaminski <michalkam12@gmail.com>
2017-11-04 16:45:51 +0100

    endlessly generate planes

---
+++ Assets/Code/PlaneData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneData
+{
+
+    public GameObject plane;
+    public float x;
+    public float y;
+    public float dirx;
+    public float diry;
+}
--- Assets/Code/generateAndFly.cs
+++ Assets/Code/generateAndFly.cs
@@ -4,36 +4,76 @@ using UnityEngine;
 
 public class generateAndFly : MonoBehaviour {
 
-    float x;
-    float y;
-    float dirx;
-    float diry;
     // Use this for initialization
-    float speed = 1.0f;
-	void Start () {
+    float speed = 2.0f;
+    List<PlaneData> planes = new List<PlaneData>();
+
+    void Start () {
         
-        float r;
-        r = Random.Range(0.0f, 1.0f);
-        if(r < 0.5f)
-        {
-            x = Random.Range(1.1f, 1.2f);
-            y = Random.Range(0.0f, 1.0f);
-            dirx = -x;
-            diry = Random.Range(0.0f, 1.0f);
-        }
-        else
-        {
-            x = Random.Range(0.0f, 1.0f);
-            y = Random.Range(1.1f, 1.2f);
-            dirx = Random.Range(0.0f, 1.0f);
-            diry = -y;
-        }
-        transform.position = Camera.main.ViewportToWorldPoint(new Vector3(x, y, 10.0f));
+        StartCoroutine(startPlanes());
     }
 	
 	// Update is called once per frame
 	void Update () {
+
+        for(int i = 0; i < planes.Count; i++)
+        {
+            move(planes[i]);
+        }
+    }
+
+    void setStartingPosition(PlaneData plane)
+    {
+        float r;
+        r = Random.Range(0.0f, 1.0f);
+        if (r < 0.5f)
+        {
+            plane.x = Random.Range(1.1f, 1.2f);
+            plane.y = Random.Range(0.0f, 1.0f);
+            plane.dirx = -plane.x;
+            plane.diry = Random.Range(0.0f, 1.0f);
+        }
+        else
+        {
+            plane.x = Random.Range(0.0f, 1.0f);
+            plane.y = Random.Range(1.1f, 1.2f);
+            plane.dirx = Random.Range(0.0f, 1.0f);
+            plane.diry = -plane.y;
+        }
+        plane.plane.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(plane.x, plane.y, 10.0f));
+    }
+
+    void move(PlaneData plane)
+    {
+        
         float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, Camera.main.ViewportToWorldPoint(new Vector3(dirx, diry, 10.0f)), step);
+        plane.plane.transform.position = Vector3.MoveTowards(plane.plane.transform.position, Camera.main.ViewportToWorldPoint(new Vector3(plane.dirx, plane.diry, 10.0f)), step);
+    }
+
+    PlaneData generatePlanes()
+    {
+        PlaneData plane = new PlaneData();
+        plane.plane = Instantiate(GameObject.Find("plane"));
+        return plane;
+    }
+
+    IEnumerator startPlanes()
+    {
+        while (true)
+        {
+            if(planes.Count == 0)
+            {
+                PlaneData plane = new PlaneData();
+                plane.plane = GameObject.Find("plane");
+                planes.Add(plane);
+                setStartingPosition(planes[0]);
+            }
+            else
+            {
+                planes.Add(generatePlanes());
+                setStartingPosition(planes[planes.Count - 1]);
+            }
+            yield return new WaitForSeconds(3);
+        }
     }
 }
